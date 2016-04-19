28dd16144fc1cc3d778aaeacd912ed6a84e7a2c5
Anders Ramsay <andersr@gmail.com>
2016-04-19 10:16:54 -0400

    cleanup

--- app/both/collections/notes.js
+++ app/both/collections/notes.js
@@ -1,6 +1,5 @@
-//This is an example of Meteor's 'magic' globals
-//We are able to create a new MongoDb collection without explicitly pointing to the dependency for this action: Mongo.  Meteor handles that for us.
+//An example of Meteor's 'magic' globals
+//We are able to create a new Mongo  collection w/o explicitly pointing to the Mongo dependency.  Meteor handles that for us.
 
-//Additionally, we are defining 'Notes' as a global variable, which will allow us to 'magically' reference it elsewhere in our app
-
+//Define 'Notes' as a global variable, allowing us to 'magically' reference it elsewhere 
 Notes = new Mongo.Collection('notes')
--- app/server/main.js
+++ app/server/main.js
@@ -1,7 +1,6 @@
 import { Meteor } from 'meteor/meteor'
 
-//Here, we see an example of ES6 arrow (=>) function syntax.
-// It is the same as function(){}, but it has an implicit return and scope is carried into the function.
+//Here, we see an example of ES6 arrow (=>) function syntax, same as function(){}, but has implicit return and carried scope
 Meteor.startup(() => {
 
 	//Here, we are 'magically' able to reference Notes that we created elsewhere.
